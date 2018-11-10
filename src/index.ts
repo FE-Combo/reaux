60b124046773c33959a07363e98401717cc32167
wangzh <605131105@qq.com>
2018-11-10 23:17:37 +0800

    upgrate to 0.0.3

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 import {render, register} from "./react/Main";
 export {Listener, LocationChangedEvent, Handler, Exception} from "./react/handler";
 export {render, register};
+export {async} from "./react/component/async";
 
 const dodoing = {
     render,
