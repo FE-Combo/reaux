03e25730f5da63c4620f87becb32fcf9cd4f35d3
Wone <edger@hellocial.com>
2018-12-05 18:02:54 +0800

    Fix WebpackChunkName magic comment invalid

--- src/module/main/component/MainLayout.tsx
+++ src/module/main/component/MainLayout.tsx
@@ -1,3 +1,6 @@
+// If unable to use import() with magic comments
+// Ref: https://github.com/airbnb/babel-plugin-dynamic-import-webpack/issues/34
+
 import React from "react";
 import {Route, Switch} from "react-router-dom";
 import Notfound from "./Notfound";
