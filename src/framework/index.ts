9cbf209f0e70e2357f4642ffc9adf50e68893cb3
vocoWone <605131105@qq.com>
2019-02-22 22:31:18 +0800

    Fix build

--- src/framework/index.ts
+++ src/framework/index.ts
@@ -3,11 +3,10 @@ import {Handler} from "./core/redux";
 import {Listener, LocationChangedEvent} from "./core/type";
 import {Exception} from "./core/Exception";
 import {async} from "./components/async";
-import {PickOptional} from "./utils/type";
 
 export type Listener = Listener;
 export type LocationChangedEvent = LocationChangedEvent;
-export {render, register, async, PickOptional, Handler, Exception};
+export {render, register, async, Handler, Exception};
 
 const Do = {
     render,
